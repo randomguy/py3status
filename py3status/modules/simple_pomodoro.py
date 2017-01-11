ba20878b25c67b0133266f36f30215ed52b34a0f
Eduard Mai <eduard.mai@posteo.de>
2017-01-12 00:06:21 +0100

    simple_pomodoro: a WIP pomodoro timer

---
+++ py3status/modules/simple_pomodoro.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# See https://github.com/ultrabug/py3status/wiki/Write-your-own-modules
+#
+# NOTE: py3status will NOT execute:
+#     - methods starting with '_'
+#     - methods decorated by @property and @staticmethod
+#
+# NOTE: reserved method names:
+#     - 'kill' method for py3status exit notification
+#     - 'on_click' method for click events from i3bar (read below please)
+#
+"""
+WIP Pomodoro technique timer for py3status.
+
+Another pomodoro timer with simpler UI
+
+Configuration parameters:
+    TODO
+Format of status string placeholders:
+    {output} output of this module
+
+@author <Eduard Mai> <eduard.mai@posteo.de>
+@license BSD
+"""
+
+from threading import Timer
+from time import time
+import logging
+from abc import ABCMeta, abstractmethod
+
+CACHE_FOREVER = -1
+POMODORO_DURATION_SEC = 5 * 3
+BREAK_DURATION_SEC = 5
+FULL_BAR = "<span font='Material Design Icons 12'>■■■■■</span>"
+
+logging.basicConfig(filename='example.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s|l.%(lineno)3d|%(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+
+
+class StoppableTimer(Timer):
+    def __init__(self, interval, *args, **kwargs):
+        super().__init__(interval, *args, **kwargs)
+        self._target_time = time() + interval
+
+    @property
+    def target_time(self):
+        return self._target_time
+
+    def is_active(self):
+        return self.target_time > time()
+
+
+class State:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, module):
+        self._module = module
+
+    @abstractmethod
+    def enter(self):
+        return
+
+    @abstractmethod
+    def exit(self):
+        return
+
+
+class TimerState(State):
+    def __init__(self, module):
+        self._timers = []
+        super().__init__(module)
+
+    @abstractmethod
+    def exit(self):
+        return
+
+    @property
+    def timers(self):
+        return self._timers
+
+    @timers.setter
+    def timers(self, new_timers):
+        self._timers = new_timers
+
+    def enter(self, duration):
+        active_timers = [t for t in self.timers if t.is_active()]
+        if not self.timers or not any(active_timers):
+            self.timers = self._module._init_timers(duration)
+            self._module.full_text = FULL_BAR
+        else:
+            self._module.full_text = self._old_text
+            for timer in active_timers:
+                timer.start()
+        self._module.py3.update()
+
+
+class StatePauseWorking(State):
+    def enter(self):
+        self._module.full_text = "<span font='Material Design Icons 12'></span> paused"
+        self._module.cached_until = CACHE_FOREVER
+        self._module.update_bar()
+        # TODO
+
+
+class StateWorking(TimerState):
+    def enter(self):
+        super().enter(POMODORO_DURATION_SEC)
+
+    def exit(self):
+        self._old_text = self._module.full_text
+        new_timers = []
+        for timer in self.timers:
+            timer.cancel()
+            new = StoppableTimer(timer.target_time - time(),
+                                 timer.function,
+                                 timer.args)
+            new_timers.append(new)
+        self.timers = new_timers
+
+
+class StateWaitForStart(State):
+    def enter(self):
+        self._module.full_text = "start <span font='Material Design Icons 12'></span>"
+        self._module.cached_until = CACHE_FOREVER
+        self._module.update_bar()
+
+
+class StateWaitForBreak(State):
+    def enter(self):
+        self._module.full_text = 'start break'
+        self._module.cached_until = CACHE_FOREVER
+
+
+class StateTakingBreak(TimerState):
+    def enter(self):
+        super().enter(BREAK_DURATION_SEC)
+
+
+class Py3status:
+    # available configuration parameters
+    cache_timeout = 10
+
+    def __init__(self):
+        wait_for_start = StateWaitForStart(self)
+        working = StateWorking(self)
+        pause_working = StatePauseWorking(self)
+        wait_for_break = StateWaitForBreak(self)
+        taking_break = StateTakingBreak(self)
+
+        # define state transitions with dict(s)
+        self._on_click_transitions = {
+            wait_for_start: working,
+            working: pause_working,
+            pause_working: working,
+            wait_for_break: taking_break}
+
+        self._on_timer_transitions = {
+            working: wait_for_break,
+            taking_break: wait_for_start}
+
+        self._state = wait_for_start
+        self._state.enter()
+
+    @property
+    def cached_until(self):
+        return self._cached_until
+
+    @cached_until.setter
+    def cached_until(self, new_cached_until):
+        self._cached_until = new_cached_until
+
+    @property
+    def full_text(self):
+        return self._full_text
+
+    @full_text.setter
+    def full_text(self, new_text):
+        self._full_text = new_text
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, new_state):
+        self._state = new_state
+
+    def _enter_next_state_on_timer(self):
+        logging.debug('SWITCHING STATE VIA TIMER!')
+        self._state = self._on_timer_transitions[self._state]
+        self._state.enter()
+        self.py3.update()
+
+    def _enter_next_state_on_click(self):
+        logging.debug('key on onclick transition:\n%s', self._state)
+        self._state.exit()
+        try:
+            self._state = self._on_click_transitions[self._state]
+        except KeyError:
+            # Stay in the same state, as now following state was defined
+            pass
+        else:
+            self._state.enter()
+            self.update_bar()
+
+    def _update_widget(self, text, cached_until):
+        """ This is executed via StoppableTimer instances"""
+        self.full_text = text
+        self.cached_until = CACHE_FOREVER
+        self.py3.update()
+
+    def _init_timers(self, duration_in_seconds):
+        """ This method will create 5 timers each firing after a fifth of
+        duration_in_seconds """
+        timer_interval = duration_in_seconds / 5
+        timers = []
+        text = FULL_BAR
+        for i in range(1, 5):
+            timer = StoppableTimer(i * timer_interval,
+                                   self._update_widget,
+                                   [text.replace('■', '□', i), time() + i*timer_interval])
+            timer.start()
+            timers.append(timer)
+        last = StoppableTimer(duration_in_seconds, self._enter_next_state_on_timer)
+        last.start()
+        timers.append(last)
+        return timers
+
+    def kill(self, i3s_output_list, i3s_config):
+        """
+        This method will be called upon py3status exit.
+        """
+        pass
+
+    def on_click(self, i3s_output_list, i3s_config, event):
+        self._enter_next_state_on_click()
+        self.update_bar()
+        """
+        This method should only be used for ADVANCED and very specific usages.
+
+        Read the 'Handle click events directly from your i3status config'
+        article from the py3status wiki:
+            https://github.com/ultrabug/py3status/wiki/
+
+        This method will be called when a click event occurs on this module's
+        output on the i3bar.
+
+        Example 'event' json object:
+        {'y': 13, 'x': 17, 'button': 1, 'name': 'example', 'instance': 'first'}
+        """
+        pass
+
+    def update_bar(self):
+        logging.debug('Executing update_bar.')
+        response = {
+            'cached_until': self.cached_until,
+            'markup': 'pango',
+            'full_text': self.full_text
+        }
+        return response
+
+
+if __name__ == "__main__":
+    """
+    Test this module by calling it directly.
+    This SHOULD work before contributing your module please.
+    """
+    from time import sleep
+    x = Py3status()
+    config = {
+        'color_bad': '#FF0000',
+        'color_degraded': '#FFFF00',
+        'color_good': '#00FF00'
+    }
+    while True:
+        print(x.update_bar())
+        sleep(3)
+# vim: expandtab
