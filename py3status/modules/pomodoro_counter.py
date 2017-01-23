7edf5b7311f5a7eb6d5555f11caf98f97e0f1fb8
Eduard Mai <eduard.mai@posteo.de>
2017-01-23 09:50:04 +0100

    pomodoro_counter: add weekly and monthly counts

--- py3status/modules/pomodoro_counter.py
+++ py3status/modules/pomodoro_counter.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-WIP a simple widget to display daily pomodoro count
+Display pomodoro count. Needs simple_pomodoro.
 
 A complementary widget for simple_pomodoro. It will parse the contents of
-~/.pomodoro.log and display the number of pomodoros completed today.
+~/.pomodoro.log (written by simple_pomodoro) and display the number of
+pomodoros completed today.
 
 Configuration parameters:
     TODO
 Format of status string placeholders:
     {output} output of this module
 
-@author <Eduard Mai> <eduard.mai@posteo.de>
+@author <randomguy>
 @license BSD
 """
-from datetime import datetime
+from datetime import datetime, timedelta
 from os import path
-# import logging
-# logging.basicConfig(filename=path.join(path.expanduser('~'), 'simple_pomodoro.log'),
-#                     level=logging.INFO,
-#                     format='%(asctime)s|l.%(lineno)3d|%(message)s',
-#                     datefmt='%m/%d/%Y %I:%M:%S %p')
 
 # POMODORO_SYMBOL = u"<span font='Material Design Icons 12'></span>"
 POMODORO_SYMBOL = u"<span font='Material Design Icons 12'></span>"
@@ -29,14 +25,25 @@ POMODORO_SYMBOL = u"<span font='Material Design Icons 12'></span>"
 class Py3status:
     def _parse_and_truncate_log(self):
         self._today = 0
+        self._week = 0
+        self._month = 0
         log_lines = []
         with open(path.join(path.expanduser("~"), '.pomodoro.log'), 'r') as fd:
-            threshold = datetime.today().replace(hour=6, minute=0, second=0)
+            threshold_today = datetime.today().replace(hour=6, minute=0, second=0)
+            threshold_week = threshold_today - timedelta(days=datetime.today().isoweekday() % 7)
+            threshold_month = threshold_today.replace(day=1)
             for line in fd:
                 pom_time = datetime.strptime(line.strip(), '[%Y-%m-%d %H:%M:%S]')
-                if pom_time > threshold:
+                if pom_time > threshold_month:
+                    self._month += 1
+                if pom_time > threshold_week:
+                    self._week += 1
+                if pom_time > threshold_today:
                     self._today += 1
-                    log_lines.append(line)
+                else:
+                    # older than a month, will be dropped
+                    continue
+                log_lines.append(line)
         # Only write back lines with times over threshold. This will truncate
         # the log so it won't grow infinitely
         with open(path.join(path.expanduser("~"), '.pomodoro.log'), 'w') as fd:
